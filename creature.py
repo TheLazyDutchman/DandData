f2ac7c0d0365f65565f5112ca0882ccfb2a7f38f
jochem pouwels <jjpouwels2004@gmail.com>
2022-01-22 11:15:09 +0100

    remove check of creatures

--- creature.py
+++ creature.py
@@ -29,7 +29,7 @@ class CreatureFactory:
             actions = [self.actionFactory(action) for action in data['actions']]
         else:
             actions = []
-            
+
         return Creature(
             name = data["name"],
             armorClass = data["armor_class"],
@@ -38,30 +38,3 @@ class CreatureFactory:
             actions = [action for action in actions if action != None])
 
 creatureFactory = CreatureFactory()
-
-
-
-import requests, json
-base_url = "https://www.dnd5eapi.co/api/"
-
-def getInfo(request):
-    sub_url = "/".join(request.split(" "))
-    if sub_url.startswith('/api/'):
-        sub_url = sub_url[5:]
-
-    url = base_url + sub_url
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        return json.loads(response.content.decode('utf-8'))
-    else:
-        return None
-monsterList = getInfo("monsters")['results']
-monsterList : list[str] = [x['index'] for x in monsterList]
-
-for monster in monsterList:
-    data = getInfo(f"monsters {monster}")
-    print(data["name"])
-    creature = creatureFactory(data)
-
-print("had all creatures")
